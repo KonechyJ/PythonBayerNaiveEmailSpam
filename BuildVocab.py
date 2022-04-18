1b97f298910e2f464a3eb4bb8d73300ddf23a5c6
Unknown <Jkonechy@live.com>
2022-04-18 15:34:47 -0700

    Added a file to build a set of vocab words to use in spam detection
    reads in from a list of emails via emails.csv and builds a vocab list of all the words in the file to help with spam detection

---
+++ BuildVocab.py
@@ -0,0 +1,33 @@
+import pandas as PD
+import nltk
+from nltk.corpus import words
+
+vocabulary = {}
+data = PD.read_csv("data/emails.csv")
+nltk.download('words')
+set_words = set(words.words())
+
+def build_vocab(current_email):
+    index = len(vocabulary)
+
+    for word in current_email:
+        if word.lower() not in vocabulary and word.lower() in set_words:
+            vocabulary[word] = index
+            index += 1
+
+if __name__ == '__main__':
+    for i in range(data.shape[0]):
+        current_email = data.iloc[i, :][0].split()
+        print(f'Currennt email is {i}/{data.shape[0]} and the /length of the vocab in {len(vocabulary)}')
+
+        build_vocab(current_email)
+
+    file = open("vocabulary.txt", "w")
+    file.write(str(vocabulary))
+    file.close()
+
+
+
+
+
+
